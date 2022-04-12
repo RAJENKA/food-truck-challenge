909ca565e344daabf76d804a67972d7abf300f51
Raj Kale <rajenka@microsoft.com>
2022-04-12 02:27:59 -0700

    Sample test for Manager

---
+++ MobileFoodFacilitiesService.Tests/FoodTruckManagerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xunit;
+using MobileFoodFacilitiesService;
+using SODA;
+using FoodTruckChallenge;
+using Moq;
+using System.Collections.Generic;
+using FoodTruckChallenge.Models;
+using System.Linq;
+
+namespace MobileFoodService.Tests
+{
+    public class FoodTruckManagerTests
+    {
+        [Fact]
+        public void FoodTruckManager_FiltersNearbyFoodTrucks()
+        {
+            //arrange
+            var sampleData = new List<FoodTruck>()
+            {
+                new FoodTruck{ Latitude = 22.1, Longitude= 122.1, Status = "approved"},
+                new FoodTruck{ Latitude = 22.111, Longitude= 122.111, Status = "approved"},
+                new FoodTruck{ Latitude = 22.111222, Longitude= 122.111222, Status = "approved"},
+                new FoodTruck{ Latitude = 22.5, Longitude= 122.5, Status = "approved"}
+            };
+
+            var foodTruckRepoMock = new Mock<IFoodTruckRepository>();
+            foodTruckRepoMock.Setup(p => p.GetFoodTruckFacilities()).Returns(sampleData);
+
+            //act
+            var manager = new FoodTruckManager(foodTruckRepoMock.Object);
+            
+            var result = manager.GetFoodTrucksWithinArea(22.1, 122.1, 1);
+
+
+            //assert
+            Assert.NotEmpty(result);
+            Assert.NotEqual(result.Count(), sampleData.Count());
+
+        }
+    }
+}
--- MobileFoodFacilitiesService.Tests/Manual/FacilitiesServiceTests.cs
+++ MobileFoodFacilitiesService.Tests/Manual/FacilitiesServiceTests.cs
@@ -10,7 +10,7 @@ namespace MobileFoodService.Tests
         [Fact]
         public void Test1()
         {
-            var sodaClient = new SodaClient("data.sfgov.org", "HxRsFepHAyIwT7Z2LdRZMwWyl");
+            var sodaClient = new SodaClient("data.sfgov.org", "");
 
             var mobileFoodService = new FacilitiesService(sodaClient);
             var result = mobileFoodService.GetFacilities();
